fb3eedd4d09f803280f2fe37152d82961d680d01
Mariana Pineda <mariana.pineda@grana-t.com>
2026-06-17 17:51:22 -0500

    fix typo

--- models/src/src_reviews.sql
+++ models/src/src_reviews.sql
@@ -11,6 +11,6 @@ select
     listing_id,
     date as review_date,
     reviewer_name,
-    comments as review_comments,
+    comments as review_text,
     sentiment as review_sentiment
 from raw_reviews
